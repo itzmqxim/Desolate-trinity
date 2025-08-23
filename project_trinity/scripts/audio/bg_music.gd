adc71708b414e62b3e67f5e16e9934ed982dbcd4
itzmqxim <itzmqxim@gmail.com>
2025-08-23 21:40:48 +0200

    Delete project_trinity directory

--- project_trinity/scripts/audio/bg_music.gd
+++
@@ -1,7 +0,0 @@
-extends AudioStreamPlayer2D
-
-func _ready():
-	# Ensure player persists across scene changes
-	process_mode = Node.PROCESS_MODE_ALWAYS
-	# Prevent audio cutting on scene changes
-	bus = "Music"  # Or your preferred audio bus
--- project_trinity/scripts/spawn_marker.gd
+++
@@ -1,7 +0,0 @@
-extends Marker2D
-
-@export var show_ing := false
-
-func _ready():
-	if not Engine.is_editor_hint() and not show_ing:
-		queue_free()
